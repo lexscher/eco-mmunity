3f63f26aa494ad34ea0152b06b89e76f9f1fbeab
Alexander Schelchere <ascellie@gmail.com>
2019-09-03 19:09:30 -0400

    change main ID name

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,12 @@
 import React, { Component, Fragment } from 'react';
 
 export default class App extends Component {
-  constructor(props) {
-    super(props)
-  }
 
   render() {
     return(
       <Fragment>
         <header className="header nav-bar">Header</header>
-        <div className="body">body tea</div>
+        <div className="body">body spill</div>
       </Fragment>
     )
   }
--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import Reactdom from 'react-dom';
 import '../sass/main.scss';
 import App from './app';
+import { createStore } from 'redux'
 
-Reactdom.render(<App />, document.getElementById('app'));
+Reactdom.render(<App />, document.getElementById('root'));
