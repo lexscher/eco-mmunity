b6e95825ee3af3db8b70e21677d9f018acdd32f8
Alexander Schelchere <ascellie@gmail.com>
2019-09-04 16:27:57 -0400

    Wrap app in Provider

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import Reactdom from 'react-dom';
 import '../sass/main.scss';
 import App from './app';
-import { createStore } from 'redux'
 
-Reactdom.render(<App />, document.getElementById('root'));
+import { store } from './store';
+
+// Necessary for Redux
+import { Provider } from 'react-redux';
+
+
+Reactdom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
