c920cf5a46f9152d327885b88fba2008b3df477c
Alexander Schelchere <ascellie@gmail.com>
2019-09-05 19:32:32 -0400

    Nav Bar Connected to store

--- src/js/NavBar/index.js
+++ src/js/NavBar/index.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { store } from '../redux/store';
 import { connect } from 'react-redux';
 
 class NavBar extends Component {
+
+
   render() {
     return (
       <nav className="nav-bar">
-        <Link to="/">Home</Link>
-        <Link to="/profile">Profile</Link>
+        <Link to="/" onClick={() => store.dispatch(this.props.changePage('default'))}>Home</Link>
         {this.props.loggedIn ? (
-          <Link>Log Out</Link>
+          [<Link to="/profile">Profile</Link>, <Link>Log Out</Link>]
         ) : (
-          <Link to="/registration">Log In</Link>
+          <Link to="/assimilate" onClick={() => store.dispatch(this.props.changePage('assimilation'))}>Log In</Link>
         )}
       </nav>
     );
