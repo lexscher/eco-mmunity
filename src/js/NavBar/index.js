44e81d6a4863d6cbf9a86c3b46a569f027f4bb5f
Alexander Schelchere <ascellie@gmail.com>
2019-09-06 10:58:29 -0400

    Navbar: map dispatch to props

--- src/js/NavBar/index.js
+++ src/js/NavBar/index.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { store } from '../redux/store';
 import { connect } from 'react-redux';
-import { pageActions } from '../redux/actions'
+import { pageActions } from '../redux/actions';
 
 class NavBar extends Component {
-
-
   render() {
     return (
       <nav className="nav-bar">
-        <Link to="/" onClick={() => store.dispatch(this.props.changePage('default'))}>Home</Link>
+        <Link to="/" onClick={() => this.props.changePage('default')}>
+          Home
+        </Link>
         {this.props.loggedIn ? (
           [<Link to="/profile">Profile</Link>, <Link>Log Out</Link>]
         ) : (
-          <Link to="/assimilate" onClick={() => store.dispatch(this.props.changePage('assimilation'))}>Log In</Link>
+          <Link
+            to="/assimilate"
+            onClick={() => this.props.changePage('assimilation')}
+          >
+            Log In
+          </Link>
         )}
       </nav>
     );
@@ -27,4 +31,7 @@ const mapDispatchToProps = {
   changePage: pageActions.changePage
 };
 
-export default connect(mapStateToProps)(NavBar);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(NavBar);
