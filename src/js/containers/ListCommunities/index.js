b1010e30047c20893dd53c5c76d62b43e81cadf4
Alexander Schelchere <ascellie@gmail.com>
2019-09-07 23:21:22 -0400

    container(list of communities): Links to each community

--- src/js/containers/ListCommunities/index.js
+++ src/js/containers/ListCommunities/index.js
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-const ListCommunities = ({ communities }) => {
-
+class ListCommunities extends Component {
+  render() {
+    let communities = this.props.communities.map(community => (
+      <Link
+        to={`/eco/${community.name}`}
+        key={community.name}
+        className="community-list-item"
+      >
+        {community.name}
+      </Link>
+    ));
+    return <div className="community-list-container">{communities}</div>;
+  }
 }
+
+const mapStateToProps = state => state;
+
+export default connect(mapStateToProps)(ListCommunities);
