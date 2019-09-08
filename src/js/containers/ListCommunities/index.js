26e5c576e8766a21d05f8758832d08016efeb31e
Alexander Schelchere <ascellie@gmail.com>
2019-09-08 12:07:06 -0400

    Toggle current community

--- src/js/containers/ListCommunities/index.js
+++ src/js/containers/ListCommunities/index.js
@@ -4,20 +4,36 @@ import { Link } from 'react-router-dom';
 import { communityActions } from '../../redux/actions';
 
 class ListCommunities extends Component {
+  state = {
+    showList: false,
+    currentCommunity: {}
+  }
+
   componentDidMount() {
     this.props.loadCommunities()
   }
+
+  toggleShowList = () => this.setState({showList: !this.state.showList})
+
+  setCurrentCommunity = community => this.setState({currentCommunity: community })
+
   render() {
     let communities = this.props.communities.map(community => (
       <Link
         to={`/eco/${community.name}`}
         key={community.name}
         className="community-list-item"
+        onClick={() => this.setCurrentCommunity(community)}
       >
         {community.name}
       </Link>
     ));
-    return <div className="community-list-container">{communities}</div>;
+    return (
+      <div>
+        <div>{this.state.currentCommunity.name ? this.state.currentCommunity.name : 'Communities' }</div>
+        <div className="community-list-container">{communities}</div>
+      </div>
+    );
   }
 }
 
