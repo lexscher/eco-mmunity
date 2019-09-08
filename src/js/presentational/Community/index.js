08ef95f7ee1b6e08cdfd920e8ed90b88f68497ac
Alexander Schelchere <ascellie@gmail.com>
2019-09-07 23:25:37 -0400

    Community: comments

--- src/js/presentational/Community/index.js
+++ src/js/presentational/Community/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
+// Will get "Current Community" that we will get when we click on
+// community List Item in the ListsCommunities container.
+// Filter posts based on community_id attribute
 const Community = ({ name, description }) => {
-  return (
-    <div className="">
-
-    </div>
-  )
-}
+  return <div className="community"></div>;
+};
