2faed9c30af4a8d32862ecd14a5a1229352ffaaa
Alexander Schelchere <ascellie@gmail.com>
2019-09-09 19:23:36 -0400

    presentational(Post): remove local state & methods

--- src/js/presentational/Post/index.js
+++ src/js/presentational/Post/index.js
@@ -5,20 +5,6 @@ import Comment from '../Comment';
 
 // This will be the modal view
 class Post extends Component {
-  state = {
-    postComments: []
-  };
-  componentDidMount() {
-    this.getPostComments();
-  }
-
-  getPostComments = () => {
-    let postComments = this.props.comments.filter(
-      comment => comment.attributes.post.id == this.props.currentPost.id
-    );
-    this.setState({ postComments });
-  };
-
   render() {
     let {
       title,
@@ -27,12 +13,12 @@ class Post extends Component {
       votes,
       voters
     } = this.props.currentPost.attributes;
-    let commentList = this.state.postComments.map(comment => <Comment comment={comment} />);
+    let commentList = this.props.currentComments.map(comment => <Comment comment={comment} />);
     return (
       <div className="post--view single-post">
         <h1>{title}</h1>
         <p>{content}</p>
-        {this.state.postComments.length > 0 ? (
+        {this.state.currentComments.length > 0 ? (
           <div className="comments-container">{commentList}</div>
         ) : (
           ''
