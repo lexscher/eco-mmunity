dbe759cecda936c32ac212cf3a93c522c5306f22
Alexander Schelchere <ascellie@gmail.com>
2019-09-09 10:50:18 -0400

    containers(ListPosts): more structured, add class names

--- src/js/containers/ListPosts/index.js
+++ src/js/containers/ListPosts/index.js
@@ -6,10 +6,21 @@ import { postActions } from '../../redux/actions';
 // dispatch the action to get the current post,
 // as well as it's comments
 const ListPosts = props => {
-  let { postList, setCurrentPost } = props
+  let { postList, setCurrentPost } = props;
   let postsList = postList.map(post => (
-    <div key={post.id} className="post-list-item--card" onClick={() => setCurrentPost(post)}>
-      <h1>{post.title}</h1> <p>{post.content}</p>
+    <div
+      key={post.id}
+      className="post-list-item--card"
+      onClick={() => setCurrentPost(post)}
+    >
+      <div className="post-info">
+        <h1 className="post-info__title">{post.title}</h1>
+        <p className="post-info__content">{post.content}</p>
+      </div>
+      <div className="post-info-extra">
+        <p className="post-info-extra__comments">comments</p>
+        <p className="post-info-extra__author">by hazel</p>
+      </div>
     </div>
   ));
   return <Fragment>{postsList}</Fragment>;
@@ -19,6 +30,9 @@ const mapStateToProps = state => state;
 
 const mapDispatchToProps = {
   setCurrentPost: postActions.setCurrentPost
-}
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(ListPosts);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ListPosts);
