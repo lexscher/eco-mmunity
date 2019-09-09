c1cc12c1f2dc3afb9de5fd4842e6a58b296080a6
Alexander Schelchere <ascellie@gmail.com>
2019-09-09 01:09:30 -0400

    Post: Prepare for Single Post View use

--- src/js/presentational/Post/index.js
+++ src/js/presentational/Post/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
+// This will be the modal view
 const Post = ({ title, content }) => {
   return (
-    <div>
+    <div className="post--view single-post">
       <h1>{title}</h1>
       <p>{content}</p>
     </div>
