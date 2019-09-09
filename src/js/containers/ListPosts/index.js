eaca27c651a72c4278bf60c686a5ec9b3abb2ca2
Alexander Schelchere <ascellie@gmail.com>
2019-09-09 01:08:53 -0400

    ListPosts: no longer uses Post component

--- src/js/containers/ListPosts/index.js
+++ src/js/containers/ListPosts/index.js
@@ -1,8 +1,15 @@
 import React, { Fragment } from 'react';
 import Post from '../../presentational/Post';
 
+// Clicking on one of these list items will
+// dispatch the action to get the current post,
+// as well as it's comments
 const ListPosts = ({ posts }) => {
-  let postsList = posts.map(post => <Post key={post.id} title={post.title} content={post.content} />);
+  let postsList = posts.map(post => (
+    <div key={post.id} className="post-list-item--card">
+      <h1>{post.title}</h1> <p>{post.content}</p>
+    </div>
+  ));
   return <Fragment>{postsList}</Fragment>;
 };
 
