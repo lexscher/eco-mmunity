67c967df2739c798c68a99b7c01aff3279216d54
Alexander Schelchere <ascellie@gmail.com>
2019-09-09 09:11:34 -0400

    containers(ListPosts): prepare to connect to redux store

--- src/js/containers/ListPosts/index.js
+++ src/js/containers/ListPosts/index.js
@@ -1,15 +1,18 @@
 import React, { Fragment } from 'react';
+import { connect } from 'react-redux';
+import { postActions } from '../../redux/actions';
 
 // Clicking on one of these list items will
 // dispatch the action to get the current post,
 // as well as it's comments
-const ListPosts = ({ posts }) => {
-  let postsList = posts.map(post => (
+const ListPosts = ({ postList }) => {
+  let postsList = postList.map(post => (
     <div key={post.id} className="post-list-item--card">
       <h1>{post.title}</h1> <p>{post.content}</p>
     </div>
   ));
   return <Fragment>{postsList}</Fragment>;
 };
+
 
 export default ListPosts;
