1ff0f6c1f8c7dce4a39dc56a26a221567e4650cd
Alexander Schelchere <ascellie@gmail.com>
2019-09-09 18:44:44 -0400

    presentational(post)

--- src/js/presentational/Post/index.js
+++ src/js/presentational/Post/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // This will be the modal view
-const Post = ({ title, content }) => {
-  return (
-    <div className="post--view single-post">
-      <h1>{title}</h1>
-      <p>{content}</p>
-    </div>
-  );
-};
+class Post extends Component {
+  render() {
+    let { title, content } = this.props;
+    return (
+      <div className="post--view single-post">
+        <h1>{title}</h1>
+        <p>{content}</p>
+      </div>
+    );
+  }
+}
 
 export default Post;
