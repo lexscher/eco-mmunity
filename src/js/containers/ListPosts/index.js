fc0318b4a014ab6ed626f1e578cadbe9ffc409c8
Alexander Schelchere <ascellie@gmail.com>
2019-09-09 12:06:06 -0400

    containers(ListPosts): uses serialized data

--- src/js/containers/ListPosts/index.js
+++ src/js/containers/ListPosts/index.js
@@ -7,22 +7,27 @@ import { postActions } from '../../redux/actions';
 // as well as it's comments
 const ListPosts = props => {
   let { postList, setCurrentPost } = props;
-  let postsList = postList.map(post => (
-    <div
-      key={post.id}
-      className="post-list-item--card"
-      onClick={() => setCurrentPost(post)}
-    >
-      <div className="post-info">
-        <h1 className="post-info__title">{post.title}</h1>
-        <p className="post-info__content">{post.content}</p>
+  let postsList = postList.map(post => {
+    let { title, content, comments, user, voters, votes } = post.attributes;
+    return (
+      <div
+        key={post.id}
+        className="post-list-item--card"
+        onClick={() => setCurrentPost(post)}
+      >
+        <div className="post-info">
+          <h1 className="post-info__title">{title}</h1>
+          <p className="post-info__content">{content}</p>
+        </div>
+        <div className="post-info-extra">
+          <p className="post-info-extra__comments">
+            {comments.length} comments
+          </p>
+          <p className="post-info-extra__author">by {user.username}</p>
+        </div>
       </div>
-      <div className="post-info-extra">
-        <p className="post-info-extra__comments">comments</p>
-        <p className="post-info-extra__author">by hazel</p>
-      </div>
-    </div>
-  ));
+    );
+  });
   return <Fragment>{postsList}</Fragment>;
 };
 
