0f9a25503be3ae827baeedbf064dfc6c174e420e
Alexander Schelchere <ascellie@gmail.com>
2019-09-09 19:18:08 -0400

    presentational(Post): shows post information and comments

--- src/js/presentational/Post/index.js
+++ src/js/presentational/Post/index.js
@@ -1,16 +1,51 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { postActions, commentActions } from '../../redux/actions';
+import Comment from '../Comment';
 
 // This will be the modal view
 class Post extends Component {
+  state = {
+    postComments: []
+  };
+  componentDidMount() {
+    this.getPostComments();
+  }
+
+  getPostComments = () => {
+    let postComments = this.props.comments.filter(
+      comment => comment.attributes.post.id == this.props.currentPost.id
+    );
+    this.setState({ postComments });
+  };
+
   render() {
-    let { title, content } = this.props;
+    let {
+      title,
+      content,
+      community,
+      votes,
+      voters
+    } = this.props.currentPost.attributes;
+    let commentList = this.state.postComments.map(comment => <Comment comment={comment} />);
     return (
       <div className="post--view single-post">
         <h1>{title}</h1>
         <p>{content}</p>
+        {this.state.postComments.length > 0 ? (
+          <div className="comments-container">{commentList}</div>
+        ) : (
+          ''
+        )}
       </div>
     );
   }
 }
 
-export default Post;
+const mapStateToProps = state => state;
+
+// const mapDispatchToProps = {
+//   loadComments: commentActions.loadComments
+// }
+
+export default connect(mapStateToProps)(Post);
